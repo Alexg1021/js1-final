0ba9df19efaa6925dde79868f0743f36e52b752d
Alexg1021 <gutierrez.lx@gmail.com>
2015-03-18 19:31:28 -0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 /**
  * Created by alexone22 on 3/16/15.
  */
+
+//contain all of the logic of what the html will do
---
+++ user.js
@@ -0,0 +1,18 @@
+/**
+ * Created by alexone22 on 3/16/15.
+ */
+
+
+(function(){
+    var User = function(first, last, userName, passWord, email, dob, gender){
+        this.first = first;
+        this.last = last;
+        this.userName = userName;
+        this.passWord = passWord;
+        this.email = email;
+        this.dob = dob;
+        this.gender = gender;
+    };
+
+}());
+
